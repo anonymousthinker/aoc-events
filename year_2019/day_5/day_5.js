167ef2544ff3ff1086a2a36b81284168e9c35573
Gour <157149036+anonymousthinker@users.noreply.github.com>
2025-02-02 15:08:17 +0530

    completed day 5 part 1 - 2019

---
+++ year_2019/day_5/day_5.js
@@ -0,0 +1,94 @@
+// Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+// Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+
+const handle3 = (data, curr) => {
+  data[data[curr + 1]] = prompt("Enter input: ");
+  return 2;
+};
+
+const handle4 = (data, mode1, curr) => {
+  if (mode1 === 1) {
+    console.log(data[curr + 1]);
+    return 2;
+  }
+
+  console.log(data[data[curr + 1]]);
+  return 2;
+};
+
+const handle2 = (data, mode1, mode2, curr) => {
+  if (mode1 === 0 && mode2 === 0) {
+    data[data[curr + 3]] =
+      Number(data[data[curr + 1]]) * Number(data[data[curr + 2]]);
+  }
+  if (mode1 === 0 && mode2 === 1) {
+    data[data[curr + 3]] =
+      Number(data[data[curr + 1]]) * Number(data[curr + 2]);
+  }
+  if (mode1 === 1 && mode2 === 0) {
+    data[data[curr + 3]] =
+      Number(data[curr + 1]) * Number(data[data[curr + 2]]);
+  }
+  if (mode1 === 1 && mode2 === 1) {
+    data[data[curr + 3]] = Number(data[curr + 1]) * Number(data[curr + 2]);
+  }
+
+  return 4;
+};
+
+const handle1 = (data, mode1, mode2, curr) => {
+  if (mode1 === 0 && mode2 === 0) {
+    data[data[curr + 3]] =
+      Number(data[data[curr + 1]]) + Number(data[data[curr + 2]]);
+  }
+  if (mode1 === 0 && mode2 === 1) {
+    data[data[curr + 3]] =
+      Number(data[data[curr + 1]]) + Number(data[curr + 2]);
+  }
+  if (mode1 === 1 && mode2 === 0) {
+    data[data[curr + 3]] =
+      Number(data[curr + 1]) + Number(data[data[curr + 2]]);
+  }
+  if (mode1 === 1 && mode2 === 1) {
+    data[data[curr + 3]] = Number(data[curr + 1]) + Number(data[curr + 2]);
+  }
+
+  return 4;
+};
+
+const traverse = (data, paraOneMode, paraTwoMode, opcode, curr) => {
+  const mode1 = paraOneMode || 0;
+  const mode2 = paraTwoMode || 0;
+
+  switch (opcode) {
+    case 1:
+      return handle1(data, mode1, mode2, +curr);
+    case 2:
+      return handle2(data, mode1, mode2, +curr);
+    case 3:
+      return handle3(data, +curr);
+    case 4:
+      return handle4(data, mode1, +curr);
+  }
+};
+
+const parseInstruction = (inst) => {
+  const padded = inst.toString().padStart(4, "0");
+  const instSplit = padded.split("");
+  const [paraTwoMode, paraOneMode, ...opcode] = instSplit;
+  return [Number(paraTwoMode), Number(paraOneMode), Number(opcode.join(""))];
+};
+
+const main = () => {
+  const data = Deno.readTextFileSync("puzzle_input.txt").split(",");
+
+  let pointer = 0;
+  while (data[pointer] !== "99") {
+    const [paraTwoMode, paraOneMode, opcode] = parseInstruction(data[pointer]);
+    pointer += traverse(data, paraOneMode, paraTwoMode, opcode, pointer);
+  }
+
+  return data;
+};
+
+console.log(main());
